8f112249effa421b18825cf623f9fbda3c2d756e
uyeeyu <ls6750@nyu.edu>
2024-12-10 00:00:48 -0500

    first commit

---
+++ click.js
@@ -0,0 +1,7 @@
+function showPopup(popupID) {
+    document.getElementById(popupID).style.display = 'flex';
+}
+
+function hidePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+}
---
+++ details.js
@@ -0,0 +1,71 @@
+const details=[
+    {   title1: "School Events",
+        description1:"Attending school events is crucial for getting familiar with your academic environment. These events can range from orientation programs to department-specific seminars and workshops. Participating early helps build a strong foundation for academic and social networking while keeping you informed about resources like career services, research opportunities, and student organizations.",
+        title2:"Networking",
+        description2:"Graduate school networking is essential for your academic and professional development. This can involve attending department meetups, conferences, or informal events where you can connect with peers, professors, and industry professionals. Building relationships early on can lead to research collaborations, internships, or job opportunities down the line.",
+        title3:"Research",
+        description3:"Begin researching potential employers and industries early. Explore companies, job roles, and skills in your field to help guide your career decisions. Understanding the job market now will prepare you for future internships or job applications.",
+        title4:"Resume Building",
+        description4:"Your resume is a living document that will need updates throughout graduate school. Start early by listing your academic achievements, any relevant work experience, and research projects. Participate in workshops or use career services to tailor your resume for future opportunities, including internships, conferences, and job applications."
+    },
+    {
+        title1:"Resume Finalization",
+        description1:"This is the time to perfect your resume. Ensure it highlights your skills, education, and any relevant experience. Tailor it for the specific industry or roles you're interested in, and get feedback from career services or mentors. Your resume should be polished and ready to send out to potential employers.",
+        title2:"Internship Applications",
+        description2:"Start applying for summer internships. These are critical for gaining experience and making connections in your field. Research opportunities, network with professionals, and submit applications early, as deadlines can come quickly.",
+        title3:"Skill Development",
+        description3:"Focus on improving skills that will make you more competitive in the job market. This could be learning new software, improving your technical expertise, or enhancing soft skills like communication and leadership. Online courses, workshops, or certifications can be great ways to build up your skillset.",
+        title4:"Mock Interviews",
+        description4:"Prepare for upcoming interviews by participating in mock interview sessions. These practice interviews help you refine your responses and improve your confidence when speaking with potential employers. Career services or trusted peers can help simulate the interview experience."
+    },
+    {
+        title1:"Internships",
+        description1:"This is your time to fully immerse yourself in an industry-related internship. Apply the knowledge you've gained from school, work on real projects, and expand your professional network. Make the most out of your experience by asking questions and seeking feedback from supervisors.",
+        title2:"Networking",
+        description2:"Take advantage of your internship or summer events to build new professional relationships. Attend networking events, connect with colleagues on LinkedIn, and stay in touch with mentors. Networking can open doors to future job opportunities.",
+        title3:"Job Search",
+        description3:"Start planning your post-graduation job search. Research companies, create a job target list, and keep an eye on job postings. Begin reaching out to contacts or recruiters in your field, even if it's early, to show interest and establish connections.",
+        title4:"Feedback and Review",
+        description4:"Collect feedback from your internship supervisors. Review your performance and identify areas for improvement. Use the summer to refine your skills and adjust your career strategy based on the feedback you receive, ensuring you are better prepared for future roles."
+    },
+    {
+        title1:"Job Applications",
+        description1:"This is the time to apply to multiple job openings in your field. Tailor each application to the specific company and role, making sure your resume, cover letter, and online profiles reflect your strengths and the skills needed for the position.",
+        title2:"Career Fairs",
+        description2:"Attend career fairs hosted by your school or professional organizations. Research companies in advance and practice introducing yourself. Prepare questions for potential employers and follow up afterward to maintain connections.",
+        title3:"Interview Preparation",
+        description3:"Get ready for interviews by reviewing common questions in your industry, practicing technical questions, and doing mock interviews. Focus on your confidence, body language, and answering in a clear, concise way.",
+        title4:"Career Counseling",
+        description4:"Meet with your school's career counselors to refine your job search strategy. They can help you with personalized advice on your resume, interview techniques, and networking strategies, as well as guide you toward the right resources and contacts."
+    },
+    {
+        title1:"Capstone Projects",
+        description1:"If your program includes a capstone project, this is a crucial time to focus on completing it. Work closely with your advisor and team members, ensuring the project aligns with your future career goals and showcases the skills you've developed throughout the program.",
+        title2:"Final Job Applications",
+        description2:"As graduation approaches, you should be finalizing and submitting any remaining job applications. Take extra care to review your materials, as these applications may be among your last before entering the workforce.",
+        title3:"Networking",
+        description3:"Continue building your professional network by staying in touch with contacts you’ve made through internships, networking events, or career fairs. This is also a good time to reconnect with any potential employers or mentors.",
+        title4:"Job Offers",
+        description4:"You may start receiving job offers during this semester. Be prepared to negotiate offers professionally, understanding your worth in the job market, and consider factors like location, salary, benefits, and company culture."
+    },
+    {
+        title1:"Capstone Projects",
+        description1:"If your program includes a capstone project, this is a crucial time to focus on completing it. Work closely with your advisor and team members, ensuring the project aligns with your future career goals and showcases the skills you've developed throughout the program.",
+        title2:"Final Job Applications",
+        description2:"As graduation approaches, you should be finalizing and submitting any remaining job applications. Take extra care to review your materials, as these applications may be among your last before entering the workforce.",
+        title3:"Networking",
+        description3:"Continue building your professional network by staying in touch with contacts you’ve made through internships, networking events, or career fairs. This is also a good time to reconnect with any potential employers or mentors.",
+        title4:"Job Offers",
+        description4:"You may start receiving job offers during this semester. Be prepared to negotiate offers professionally, understanding your worth in the job market, and consider factors like location, salary, benefits, and company culture."
+    },
+    {
+        title1:"Legal Procedure",
+        description1:"After graduation, ensure you're aware of any legal processes you need to complete, such as applying for a work visa (e.g., OPT or H-1B if you're an international student), filing taxes, or securing any certifications relevant to your field.",
+        title2:"Onboarding",
+        description2:"When starting a new job, you'll go through an onboarding process, which includes understanding company policies, completing HR documentation, and getting to know your role and team. This sets the foundation for your success in the company.",
+        title3:"Alumni Engagement",
+        description3:"Stay engaged with your university's alumni network. Alumni connections can offer advice, job leads, and networking opportunities, plus being involved in alumni events can keep you updated on industry trends and opportunities.",
+        title4:"Goal Setting",
+        description4:"As you transition into your career, set clear short- and long-term goals. This can include professional development, certifications, or moving up the career ladder. Regularly revisiting your goals helps ensure continued growth and satisfaction in your career."
+    },
+]
